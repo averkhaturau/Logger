edd5adda648cbc51d8003b5c8e8c19107533a3ff
averkhaturau <aliaksei.verkhaturau@gmail.com>
2017-04-16 15:12:38 +0300

    missed file

---
+++ logger.cpp
@@ -0,0 +1,46 @@
+﻿#include "logger.h"
+
+
+#ifdef _WIN32
+#include <Windows.h>
+#include <Shlobj.h>
+#endif
+
+std::mutex Logger::LogRecord::allowOnlyOneRecord;
+
+Logger& Logger::instance()
+{
+    auto getLogfilename = []() {
+#ifdef _WIN32
+        wchar_t logfileDir[MAX_PATH] = {};
+        SHGetFolderPathW(0, CSIDL_LOCAL_APPDATA | CSIDL_FLAG_CREATE, NULL, SHGFP_TYPE_CURRENT, reinterpret_cast<LPWSTR>(logfileDir));
+        return std::tr2::sys::path(toUtf8(logfileDir)) / std::tr2::sys::path(BRAND_COMPANYNAME "/" BRAND_NAME "/logfile.txt");
+#else
+        return std::tr2::sys::path("logfile.txt");
+#endif
+    };
+    static Logger l2f(getLogfilename());
+    return l2f;
+}
+
+Logger::LogRecord::LogRecord(Logger& l2f, bool dub2console) : my_l2f(l2f), duplicateToConsole(dub2console)
+{
+    allowOnlyOneRecord.lock();
+    my_l2f << timestamp(std::chrono::system_clock::now());
+}
+
+Logger::LogRecord::LogRecord(LogRecord&& a) : my_l2f(a.my_l2f), duplicateToConsole(std::move(a.duplicateToConsole))
+{
+    a.shouldFlush = false;
+}
+
+Logger::LogRecord::~LogRecord()
+{
+    if (shouldFlush) {
+        // must put line break here to be thread-safe
+        my_l2f << "\n"; my_l2f.flush();
+        if (duplicateToConsole)
+            std::cout << std::endl;
+        allowOnlyOneRecord.unlock();
+    }
+}
